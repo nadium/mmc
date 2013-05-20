e65a3e948d6dd712d69914fdf15d493cba2d9272
MarionGeorges <marion.georges@yahoo.fr>
2013-05-20 17:00:33 +0200

    Ajout de 3 catégories de Hall Of fame : Music, Food, Photo

--- HallFame/fame.php
+++ HallFame/fame.php
@@ -6,9 +6,11 @@
 </head>
 <body>
 
-	<div style="width:500px; height:500px; background-color:silver;">
+	<div style="width:500px; height:auto; background-color:silver;">
 
 		<?php
+		/* HALL OF FAME POINT*/
+
 		include("db.php");
 		$sqluser="SELECT * FROM User ORDER BY US_point DESC;";
 		$resuser=mysql_query($sqluser);
@@ -18,12 +20,60 @@
 		while($tabuser=mysql_fetch_array($resuser)){
 			echo $tabuser["US_twitter"];
 			echo " | ";
-			echo $tabuser["US_point"]." cp";
-			echo "<hr/>";
+			echo $tabuser["US_point"]." point";
+			echo "<hr/>";		
+		}
+echo "<br/><br/><br/>";
 
-			
+		/* HALL OF FAME MUSIC*/
+
+		include("db.php");
+		$sqluser="SELECT * FROM User ORDER BY US_music DESC;";
+		$resuser=mysql_query($sqluser);
+
+		//echo $sqluser;
+
+		while($tabuser=mysql_fetch_array($resuser)){
+			echo $tabuser["US_twitter"];
+			echo " | ";
+			echo $tabuser["US_music"]." pointmusic";
+			echo "<hr/>";		
 		}
 
+echo "<br/><br/><br/>";
+
+		/* HALL OF FAME FOOD*/
+
+		include("db.php");
+		$sqluser="SELECT * FROM User ORDER BY US_food DESC;";
+		$resuser=mysql_query($sqluser);
+
+		//echo $sqluser;
+
+		while($tabuser=mysql_fetch_array($resuser)){
+			echo $tabuser["US_twitter"];
+			echo " | ";
+			echo $tabuser["US_food"]." pointfood";
+			echo "<hr/>";		
+		}
+echo "<br/><br/><br/>";
+
+		/* HALL OF FAME PHOTO*/
+
+		include("db.php");
+		$sqluser="SELECT * FROM User ORDER BY US_photo DESC;";
+		$resuser=mysql_query($sqluser);
+
+		//echo $sqluser;
+
+		while($tabuser=mysql_fetch_array($resuser)){
+			echo $tabuser["US_twitter"];
+			echo " | ";
+			echo $tabuser["US_photo"]." pointphoto";
+			echo "<hr/>";		
+		}
+
+echo "<br/><br/><br/>";
 
 		?>
 	</div>	
