58ed51ebe14471dbfbc7da5a0478365d617a31c9
nadium <philinh.ngo@gmail.com>
2013-06-02 00:22:25 +0200

    Inté Home

--- Integration/index.php
+++ Integration/index.php
@@ -10,25 +10,88 @@
 	<nav>
 		<div id="nav-wrapper">
 			<div id="logo">&nbsp;</div>
-			<ul>
-				<li>MY PROFILE</li>
-				<li>HALL OF FAME</li>
-				<li>ABOUT</li>
-				<li></li>
-			</ul>
-			<ul>
-				<li>Sign Up</li>
-				<li>Log In</li>
-			</ul>
+			<div>
+				<a class="first" href="">MY PROFILE</a>
+				<a href="">HALL OF FAME</a>
+				<a href="">ABOUT</a>
+			</div>
+			<div>
+				<a class="first" href="">Sign Up</a>
+				<a href="">Log In</a>
+			</div>
 		</div>
 	</nav>
 	<header>
+		<div id="tmc-wrapper">
+			<div id="tmc">
+				<p>It’s cute, it’s gentle, it’s on your phone</p>
+				<div><a href="">Download your creature</a></div>
+			</div>
+			
+			<div class="illustration">&nbsp;</div>
+		</div>
+
 	</header>
 	<section id="content">
-		<div></div>
-		<div></div>
-		<div></div>
+		<div id="first-block">
+			<div>A fun way to tweet about what you like</div>
+		</div>
+		<div id="second-block">
+			<div>
+				<div id="profil">
+					Access your customized<br>profile and creature
+				</div>
+				<div id="hof">
+					Win your place in<br>the Hall of Fame
+				</div>
+			</div>
+		</div>
+
+		<div id="third-block">
+			<div>
+				<h2>HOW DOES IT WORK?</h2>
+				<div class="creature">
+					<img src="css/images/creature-old.png" alt="Creature">
+					<p>At each tweet, your creature grows and learn like a kid.
+						Depending on what you tweet, she may evolve in a certain way.
+						The creature has her own moods, based on what she learns or if 
+						you don’t tweet regularly.
+						It’s up to you to treat her well!
+					</p>
+				</div>
+				<div class="creature">
+					<p>The more you tweet, the more your creature will grow and learn.
+						Depending on the information you share, she will turn into 
+						something specific to its owner.
+						If you do not tweet often the creature will lose health points and
+						her mood will change and you will have a hard time healing her.</p>
+						<img src="css/images/creature-new.png" id="oldCreature" alt="Creature">
+				</div>
+				<div class="creature">
+					<img src="css/images/creature-evolved.png" alt="Creature">
+					<p>When tweeting, you earn points.
+						The creature evolves based on this amount of points.
+						Gather the more points you can and claim your place
+						to the Hall of Fame !
+					</p>
+				</div>
+				<div class="button"><a href="">Get your own!</a></div>
+			</div>
+		</div>
 	</section>
-	<footer></footer>
+	<footer>©Tweet my creature</footer>
 </body>
 </html>
+
+
+
+
+
+
+
+
+
+
+
+
+
